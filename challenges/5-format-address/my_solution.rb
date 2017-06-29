52888e7550c9a331c2e7176834cbfe8904c9811c
gato-cosmico <elgatocosmico66@gmail.com>
2017-06-28 20:24:58 -0500

    aprendiendo a leer codigo

--- challenges/5-format-address/my_solution.rb
+++ challenges/5-format-address/my_solution.rb
@@ -1,3 +1,7 @@
 # Format an Address
 
 # Tu solucion abajo:
+def make_address(street, city, state, zip) 
+puts "You live at #{street} in the beautiful city #{city}, #{state} Your zip is #{zip}" 	
+end
+

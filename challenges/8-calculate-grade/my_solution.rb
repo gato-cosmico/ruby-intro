dcf7fc1cd6fc45913a618985cd05ee3845bbede3
gato-cosmico <elgatocosmico66@gmail.com>
2017-06-29 18:59:09 -0500

    usar expresiones condicionales

--- challenges/8-calculate-grade/my_solution.rb
+++ challenges/8-calculate-grade/my_solution.rb
@@ -2,3 +2,17 @@
 
 # Tu solucion abajo:
 
+def get_grade(average)
+	if average >=90
+		puts "a"
+	elsif average >=80
+		puts "b"
+	elsif average >=70
+		puts "c"
+	elsif average >=60
+		puts "d"
+	elsif average <60
+		puts "f"
+	end 
+end 
+
